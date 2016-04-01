89a757945a894f76cc15956de6d659e3f7af3d23
Atze Dijkstra <atzedijkstra@gmail.com>
2016-04-01 07:54:39 +0200

    added options for help and succes on workleft

--- src/Main.hs
+++ src/Main.hs
@@ -1,28 +1,46 @@
+{-# LANGUAGE TemplateHaskell #-}
+
 module Main where
 
 import System.Environment
 import System.Console.GetOpt
 import System.Exit
+import Control.Monad
 
+import UHC.Util.Lens
 import UHC.Util.CHR.Solve.TreeTrie.Examples.Term.Main
+
+-- | Immediate quit options
+data ImmQuit
+  = ImmQuit_Help
 
 -- | Program options
 data Opts
   = Opts
-      { optVerbose :: Bool
+      { _optVerbose                 :: Bool
+      , _optSucceedOnLeftoverWork   :: Bool
+      , _optImmQuit                 :: [ImmQuit]
       }
+
+mkLabel ''Opts
 
 defaultOpts :: Opts
 defaultOpts
   = Opts
-      { optVerbose = False
+      { _optVerbose                 = False
+      , _optSucceedOnLeftoverWork   = False
+      , _optImmQuit                 = []
       }
 
 -- | Options for 'getOpt'
 options :: [OptDescr (Opts -> Opts)]
 options =
-    [ mk ['v'] ["verbose"] "extra output, debugging output"
-         (NoArg $ \o -> o {optVerbose = True})
+    [ mk "v" ["verbose"] "extra output, debugging output"
+         (NoArg $ optVerbose ^= True)
+    , mk "s" ["succeed-on-leftover"] "left over unresolvable (non residue) work is also a succesful result"
+         (NoArg $ optSucceedOnLeftoverWork ^= True)
+    , mk "h" ["help"] "print this help"
+         (NoArg $ optImmQuit ^$= (ImmQuit_Help :))
     ]
   where
     mk so lo desc o = Option so lo o desc
@@ -30,12 +48,28 @@ options =
 main = do
     args <- getArgs
     progname <- getProgName
+
     case getOpt Permute options args of
+       -- options ok
        (o,[fn],[]) -> do
            let opts = foldl (flip id) defaultOpts o
-           runFile (if optVerbose opts then [RunOpt_DebugTrace] else []) fn
+           case _optImmQuit opts of
+             imm@(_:_) -> forM_ imm $ \iq -> case iq of
+               ImmQuit_Help -> printUsage progname []
+
+             -- no immediate quit options
+             _ -> do
+               flip runFile fn $ 
+                 (if _optVerbose opts then [RunOpt_DebugTrace] else []) ++
+                 (if _optSucceedOnLeftoverWork opts then [RunOpt_SucceedOnLeftoverWork] else [])
+
        (_,_,errs) -> do
-           putStrLn $ concat errs ++ usageInfo header options
+           printUsage progname errs
            exitFailure
-         where header = "Usage: " ++ progname ++ " [OPTION...] file\n\nOptions:"
+
     return ()
+
+  where
+    printUsage progname errs = putStrLn $ concat errs ++ usageInfo (header progname) options
+    header progname = "Usage: " ++ progname ++ " [OPTION...] file\n\nOptions:"
+
